b3b8c89b8df32e0d57cf2fec910da43f43e6e7ab
Eric Windmill <ewindmill@google.com>
2023-01-23 11:49:47 +0300

    zero state screen

--- lib/src/shared/auth.dart
+++ lib/src/shared/auth.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
+import 'package:myartist/src/shared/auth_highlight.dart';
 
 import '../features/home/home.dart';
 import 'router.dart';
@@ -13,28 +13,28 @@ class AuthGate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
+      builder: (context, asyncSnapshot) {
+        if (!asyncSnapshot.hasData) {
           return Scaffold(
             appBar: AppBar(
               centerTitle: false,
-              title: const Text('Good morning'),
+              toolbarHeight: kToolbarHeight * 2,
               actions: const [BrightnessToggle()],
+              title: Text('Good Morning'),
             ),
-            body: SignInScreen(
-              headerBuilder: (context, constraints, shrinkOffset) {
-                return Center(
-                  child: Text(
-                    'To see your favorite artists, please sign in.',
-                    style: Theme.of(context).textTheme.titleLarge,
+            body: Column(
+              children: [
+                AuthGuardHighlight(),
+                Flexible(
+                  child: SignInScreen(
+                    providers: [
+                      GoogleProvider(
+                        clientId:
+                            '1007448672439-6d3mlihnvjf0jemmifgccqhtsdj02rk1.apps.googleusercontent.com',
+                      )
+                    ],
                   ),
-                );
-              },
-              providers: [
-                GoogleProvider(
-                    clientId:
-                        '1007448672439-6d3mlihnvjf0jemmifgccqhtsdj02rk1.apps.googleusercontent.com'),
+                ),
               ],
             ),
           );
