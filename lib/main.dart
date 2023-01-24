71cf07a045b96e1b99e894d68ffef7baab0e5695
Eric Windmill <ewindmill@google.com>
2023-01-24 09:19:58 +0300

    create signed in branch

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,24 @@
 // found in the LICENSE file.
 
 import 'package:desktop_window/desktop_window.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:universal_platform/universal_platform.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 import 'src/shared/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: "firebase.ff@gmail.com", password: "f1reb@s3");
   if (UniversalPlatform.isDesktop) {
     setDesktopWindow();
   }
 
-  // FirebaseAuth.instance.signOut();
   runApp(const MyApp());
 }
 
